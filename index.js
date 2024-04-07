f5b7073ca36195176f3560d0e2b84382260dc5e4
Guilherme - Maka <92003604+1maka3@users.noreply.github.com>
2024-04-07 18:19:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+// Sidebar
+const menuItems = document.querySelectorAll('.menu-item');
+
+// Messages 
+const messageNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+const changeActiveItem = () => {
+    menuItems.forEach(item => {
+        item.classList.remove('active');
+    })
+}
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        changeActiveItem();
+        item.classList.add('active');
+        if(item.id != 'notifications') {
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
+        } else {
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('#notifications .notification-count').
+            style.display = 'none';
+        }
+    })
+})
+
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1) {
+            user.style.display = 'flex'; 
+        } else {
+            user.style.display = 'none';
+        }
+    })
+}
+
+messageSearch.addEventListener('keyup', searchMessage);
+
+messageNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messageNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+// Adicionando um ouvinte de eventos para o botão de alternância de tema
+document.getElementById('toggle-theme-btn').addEventListener('click', function() {
+    // Alternando entre os temas claro e escuro
+    document.body.classList.toggle('dark-theme');
+    
+    // Salvando a preferência do usuário no localStorage
+    if (document.body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
+// Verificando se o usuário já possui uma preferência de tema no localStorage
+window.onload = function() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+};
