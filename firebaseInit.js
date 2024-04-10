bb1918836246f54c680227c4c4e8580dd76ccd84
BrinooTk <126296596+BrinooTk@users.noreply.github.com>
2024-04-10 18:54:58 -0300

    update

---
+++ firebaseInit.js
@@ -0,0 +1,14 @@
+// Inicialização do Firebase
+if (!firebase.apps.length) {
+    var firebaseConfig = {
+      apiKey: "AIzaSyB0SpAb1AlcUfK21EmyQkMPKRbZNsARK0Y",
+      authDomain: "petbook-de89b.firebaseapp.com",
+      projectId: "petbook-de89b",
+      storageBucket: "petbook-de89b.appspot.com",
+      messagingSenderId: "86363192720",
+      appId: "1:86363192720:web:b3e726bb41de3ba9074715",
+      measurementId: "G-7BF22VHZD5"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+  
